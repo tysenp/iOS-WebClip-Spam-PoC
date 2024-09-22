4fcd453ba1c07a07e7e17cae255102d581033177
tysen. <tysen@tutanota.de>
2024-09-22 00:23:30 -0400

    Update autoCreate.py

--- autoCreate.py
+++ autoCreate.py
@@ -1,7 +1,7 @@
 # Please read "Usage" first in the Read Me!
 # You can use this script to easily create hundreds/thousands of WebClips.
 # To start copy and paste the entire <dict> block within the <array> to a text file named "mobileConfig.txt" and set the vaule of <key>PayloadIdentifier</key> to "[ID]" and the vaule of <key>PayloadUUID</key> to "[UUID]".
-# And that's it, run and you will recieve your .mobileconfig file, from there add the following necessary code to the top:
+# Now run and you will recieve your .mobileconfig file, from there add the following necessary code to the top:
 
 # <?xml version="1.0" encoding="UTF-8"?>
 # <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -12,6 +12,11 @@
 
 # And add the following to the bottom:
 
+# </array>
+# </dict>
+# </plist>
+
+# Perfect, your configuration profile is now ready to be tested!
 
 import uuid
 
